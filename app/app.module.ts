bb6bb464cab9ec8f4147b4a327bdfec833670810
Daniel Eaton <danieleaton747@gmail.com>
2016-11-17 08:33:22 -0700

    added Hero Class, my-app template, metadata

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,3 +1,18 @@
 /**
  * Created by Daniel Eaton on 11/16/2016.
  */
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }  from './app.component';
+@NgModule({
+	imports: [
+		BrowserModule,
+		FormsModule
+	],
+	declarations: [
+		AppComponent
+	],
+	bootstrap: [ AppComponent ]
+})
+export class AppModule { }
--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,7 @@
 /**
  * Created by Daniel Eaton on 11/16/2016.
  */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
